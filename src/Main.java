817eb65fe1ce23ac9b0c3c1762f0a4f08d134ad2
Rosty <rostmaster007@gmail.com git config --global user.email rostmaster007@gmail.com>
2024-05-24 15:50:43 +0300

    Java Lesson 1 HW

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        JavaLesson1.run(); // System.out.println() function used and understood ;
+//        JavaHW1.PDF1run(); // Primitive Variables
+        JavaHW1.PDF2run(); // Operators and Expressions
+    }
+
+}
