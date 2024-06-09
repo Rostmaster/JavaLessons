bfc3aec063093234aff70d826fb3ad6d7e77e711
Rosty <rostmaster007@gmail.com git config --global user.email rostmaster007@gmail.com>
2024-06-09 21:42:12 +0300

    Hierarchy and interfaces

---
+++ src/Hierarchy/Movable.java
@@ -0,0 +1,11 @@
+package Hierarchy;
+
+public interface Movable {
+    void moveUp();
+
+    void moveDown();
+
+    void moveLeft();
+
+    void moveRight();
+}
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,14 @@
+import Hierarchy.MovablePoint;
+
 public class Main {
 
     public static void main(String[] args) {
-        //Recursion
-        System.out.println("Recursion");
-        System.out.println(JavaRecursion.multiplyRange(4, 6));//120
-        System.out.println(JavaRecursion.multiplyRange(2, 7));//5040
+        MovablePoint point = new MovablePoint(0, 0, 3, 2);
+        System.out.println(point);
 
-        //Classes
-        System.out.println("Classes");
-        //Student create
-        Student Oleg = new Student("Oleg", 18, true, "Who knows", "Too Russian",
-                "Accent indentation", "TLV university of languages and computer sciences");
-        System.out.println(Oleg);
-
-        //Course create
-        Course FullStack = new Course("FullStack", 20, "Itay Hauptman");
-        Oleg = FullStack.registerStudent(Oleg).student;
-
-        System.out.println("\nOleg registered Fullstack\n");
-        System.out.println(Oleg);
-
-        System.out.println("\nFullstack course printing:\n");
-        System.out.println(FullStack);
-
+        point.moveLeft();
+        point.moveUp();
+        System.out.println(point);
     }
 
 
